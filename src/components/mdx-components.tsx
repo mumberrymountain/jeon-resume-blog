2df745fb48fb48a1c62cac7aee615c67df53a231
Jeon Insoo <mumberrymountain@gmail.com>
2026-06-27 22:02:44 +0900

    fix: 이미지 렌더링 오류 수정

--- src/components/mdx-components.tsx
+++ src/components/mdx-components.tsx
@@ -1,5 +1,6 @@
 import type { MDXComponents } from "mdx/types";
 import Link from "next/link";
+import { assetUrl } from "@/lib/asset";
 
 // MDX 본문에서 사용할 커스텀 컴포넌트 매핑입니다.
 // 대부분의 스타일은 detail 페이지의 `prose` 클래스가 처리하므로
@@ -19,5 +20,10 @@ export const mdxComponents: MDXComponents = {
         {children}
       </a>
     );
+  },
+  img: ({ src = "", alt = "", ...props }) => {
+    const resolvedSrc = src.startsWith("/") ? assetUrl(src) : src;
+    // eslint-disable-next-line @next/next/no-img-element
+    return <img src={resolvedSrc} alt={alt} {...props} />;
   },
 };
